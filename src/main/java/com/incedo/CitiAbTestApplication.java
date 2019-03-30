9aa229936f3bf5a7d7aa35e6d800794d20d17848
deb-patra <patra.deb@gmail.com>
2019-03-29 21:42:38 -0500

    Initial commit

---
+++ src/main/java/com/incedo/CitiAbTestApplication.java
@@ -0,0 +1,13 @@
+package com.incedo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiAbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiAbTestApplication.class, args);
+	}
+
+}
